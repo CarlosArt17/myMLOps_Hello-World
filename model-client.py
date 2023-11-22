b589121690bce333494aef82929cc662837b4b88
carlosart17 <zs200006730@estudiantes.uv.mx>
2023-11-21 22:15:40 -0600

    publicar modelo

--- model-client.py
+++ model-client.py
@@ -1,28 +1,39 @@
-import json
 import streamlit as st
 import requests
 
+# URL del servidor donde está desplegado el modelo
 SERVER_URL = 'https://linear-model-service-carlosart17.cloud.okteto.net/v1/models/linear-model:predict'
 
-def make_prediction(inputs):
-    predict_request = {'instances': inputs}
-    response = requests.post(SERVER_URL, json=predict_request)
-    response.raise_for_status()
-    prediction = response.json()
-    return prediction
+def make_prediction(x_value):
+    try:
+        predict_request = {'instances': [[x_value]]}
+        response = requests.post(SERVER_URL, json=predict_request)
+        response.raise_for_status()
+        prediction = response.json()
+        return prediction["predictions"][0][0]
+    except Exception as e:
+        st.error(f'Error al hacer la predicción: {str(e)}')
+        return None
 
 def main():
-    st.title('Estimador de Tiempo de Desarrollo')
+    # Agrega el archivo de estilo CSS
+    st.markdown(
+        '<style>' + open('styles.css').read() + '</style>', 
+        unsafe_allow_html=True
+    )
 
-    code_size = st.number_input('Ingrese el numero de lineas de codigo:', min_value=0.0, step=1.0)
+    st.title('Formula 2x + 1 = 11')
+
+    # Ingresar el valor de x
+    x_value = st.number_input('Ingrese el valor de x:')
 
     if st.button('Predecir'):
-        # Ajustar la entrada para que coincida con la nueva ecuación
-        # En este caso, la entrada debe ser [2 * code_size + 1]
-        inputs = [2 * code_size + 1]
+        # Hacer la predicción
+        prediction = make_prediction(x_value)
 
-        prediction = make_prediction(inputs)
-        st.write(f'Tiempo estimado de desarrollo para un tamaño de código en horas de {code_size}: {prediction["predictions"][0][0]}')
+        if prediction is not None:
+            # Mostrar la predicción
+            st.write(f'Para x = {x_value}, la predicción es: {prediction}')
 
 if __name__ == '__main__':
     main()
